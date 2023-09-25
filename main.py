b3f58693cddb3427d64666db665b998842102829
denys <supriadkin.d@gmail.com>
2023-09-25 13:50:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import PhotoImage
+
+app = tk.Tk()
+app.geometry("800x600")
+app.title('Holidays Calendar')
+app.configure(background='#272427')
+
+
+
+app.mainloop()
